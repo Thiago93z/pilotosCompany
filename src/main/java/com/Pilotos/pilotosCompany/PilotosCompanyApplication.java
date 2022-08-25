2b7f754205de1e376346a1dfd1f7a5cea842d601
Thiago93z <sfamado88577@umanizales.edu.co>
2022-08-25 10:23:15 -0500

    Initial commit

---
+++ src/main/java/com/Pilotos/pilotosCompany/PilotosCompanyApplication.java
@@ -0,0 +1,19 @@
+package com.Pilotos.pilotosCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotosCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotosCompanyApplication.class, args);
+		Empresa pilotos = new Empresa("Pilotos", "Armenia 123", 1234567, "989-998789" );
+		System.out.println(pilotos.getNombre());
+		pilotos.setNIT("4567890");
+		System.out.println(pilotos.getNIT());
+		pilotos.setDireccion("Av bolivar 15-58");
+		System.out.println(pilotos.getDireccion());
+	}
+
+}
---
+++ src/test/java/com/Pilotos/pilotosCompany/PilotosCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pilotos.pilotosCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotosCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

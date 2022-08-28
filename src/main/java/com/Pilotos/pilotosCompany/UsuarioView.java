2523d4ea20d08b0ed44a989c61e6ce5ef7cdfe4c
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2022-08-28 12:26:43 -0500

    Create UsuarioView.java

---
+++ src/main/java/com/Pilotos/pilotosCompany/UsuarioView.java
@@ -0,0 +1,18 @@
+package com.Pilotos.pilotosCompany.Vista;
+
+
+public class UsuarioView {
+    public void mostrarUsuarios(String nombre,String direccion,String telefono,String documento){
+        System.out.println("Nombre del usuario: "+ nombre);
+        System.out.println("Direccion: "+ direccion);
+        System.out.println("Telefono: "+ telefono);
+        System.out.println("Documento: "+ documento);
+
+    }
+    public void verUsuario(){
+
+    }
+    public void crearEliminarUsuario(){
+
+    }
+}

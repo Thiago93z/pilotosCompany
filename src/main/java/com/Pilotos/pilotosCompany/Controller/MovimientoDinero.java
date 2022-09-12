35596380da797c2ac09d0925a55e2f5a802060ef
Sonia Reyes <reyezonia@gmail.com>
2022-09-12 16:05:35 -0500

    Update MovimientoDinero.java
    Se agrega el llamado de mensajes de confirmación a los servicios

--- src/main/java/com/Pilotos/pilotosCompany/Controller/MovimientoDinero.java
+++ src/main/java/com/Pilotos/pilotosCompany/Controller/MovimientoDinero.java
@@ -15,16 +15,54 @@ public class MovimientoDineroController {
     MovimientoDineroService movimientoDineroService;
 
     @GetMapping({"/","/verMovimientoDinero"})
-    public  String viewMovimientoDinero(Model model){
+    public  String viewMovimientoDinero(Model model, @ModelAttribute("mensaje") String mensaje){
         List<MovimientoDinero> listaMovimientoDinero = movimientoDineroServ.getAllMovimientoDinero();
         model.addAttribute("movimientoDineroList", listaMovimientoDinero);
         return "verMovimientoDinero";
     }
     
      @GetMapping({"/AgregarMovimientoDinero"})
-    public  String viewAddMovimientoDinero(Model model){
+    public  String AddMovimientoDinero(Model model){
         MovimientoDinero movDin = new MovimientoDinero();
         model.addAttribute("movDin", movDin);
         return "agregarMovimientoDinero"; 
+    }
+    
+       @PostMapping({"/GuardarMovimientoDinero"})//servicio
+    public  String SaveMovimientoDinero(MovimientoDinero movimientoDinero, RedirectAttributes redirectAttributes) {
+        if(movimientoDineroService.actualizarMovimientoDinero(movimientoDinero)==true){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
+            return "redirect:/VerMovimientoDinero";
+            //redireccionamiento se hace a aservicios
+        }
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
+        return "redirect:/AgregarMovimientoDinero";
+    }
+      @PostMapping("/ActualizarMovimientoDinero")
+    public String updateMovimientoDinero(@ModelAttribute("movimientoDinero") MovimientoDinero movimientoDinero, RedirectAttributes redirectAttributes){
+        if(movimientoDineroService.actualizarMovimientoDinero(movimientoDinero)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
+            return "redirect:/VerMovimientoDinero";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/EditarMovimientoDinero/"+ movimientoDinero.getId();
+
+    }
+    @GetMapping("/EditarMovimientoDinero/{id}")
+    public String editarMovimientoDinero(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        MovimientoDinero movimientoDinero = movimientoDineroService.getMovimientoDinero(id);
+        model.addAttribute("MovimientoDinero", movimientoDinero);
+        model.addAttribute("mensaje", mensaje);
+        return "editarEmpresa";
+    }
+
+    @GetMapping("/EliminarMovimientoDinero/{id}")
+    public String eliminarMovimientoDinero(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if (movimientoDineroService.eliminarMovimientoDinero(id)==true){
+            redirectAttributes.addFlashAttribute("mensaje","deleteOK");
+            return "redirect:/VerMovimientoDinero";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "deleteError");
+        return "redirect:/VerMovimientoDinero";
     }
 }

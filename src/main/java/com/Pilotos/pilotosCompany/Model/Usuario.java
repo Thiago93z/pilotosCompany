0c0a8853bdbaed131a66d8dd516caf3e1f80f3e9
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2022-09-11 21:50:14 -0500

    Create Usuario.java

---
+++ src/main/java/com/Pilotos/pilotosCompany/Model/Usuario.java
@@ -0,0 +1,72 @@
+package com.Pilotos.pilotosCompany.Model;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name="Usuario")
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name="nombre", nullable = false)
+    private String nombre;
+    @Column(name="direccion", nullable = false)
+    private String direccion;
+    @Column(name="telefono", nullable = false)
+    private String telefono;
+    @Column(name="documento", nullable = false)
+    private String documento;
+  
+    @ManyToOne(fetch = FetchType.LAZY , targetEntity = com.Pilotos.pilotosCompany.Model.Empresa.class)
+    @JoinColumn(name = "IdEmpresa")
+    private Empresa Empresa;
+
+
+
+    public usuario(){
+
+    }
+
+
+    public usuario(String nombre,String direccion, String telefono, String documento){
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+}
